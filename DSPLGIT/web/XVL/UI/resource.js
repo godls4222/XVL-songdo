9eafbe7dc3d3b0663be7b637f36e5e5df8c2f658
hijung-i <hijung@data-flow.co.kr>
2021-07-27 11:24:10 +0900

    add resource.js

---
+++ DSPLGIT/web/XVL/UI/resource.js
@@ -0,0 +1,3 @@
+var XVL_WEB3D_ERR_LOADING_MODEL_TOOLBAR = "モデル読み込み中です。ツールバーの操作はできません。";
+var XVL_WEB3D_ERR_LOADING_MODEL_FAILURE_MAIN = "以下のファイルの読み込みに失敗しました。\n\n";
+var XVL_WEB3D_ERR_LOADING_MODEL_FAILURE_DETAIL = "\n\n[エラーの詳細情報]\n";
